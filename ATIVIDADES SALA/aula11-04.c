2b044edf1bcc1f6d599720d5a795a84392d014fc
Caio Vinicius Silva Rodrigues <caiovsrodriguess@gmai.com>
2023-04-11 21:08:02 -0300

    .

--- ATIVIDADES SALA/aula11-04.c
+++ ATIVIDADES SALA/aula11-04.c
@@ -8,15 +8,11 @@ int convertTempCelsius(int graus){
 
 }
 
-int convertTempFahrenheit(int fah){
-  int convert2;
-  convert2 = (fah - 32) * 5 /9;
-  return (convert2);
-}
+
 
 int main(){
 
-    int graus, fah, convert2, convert;
+    int graus, convert;
 
     printf("Digite quantos graus deseja converter de Celsius para Fah: ");
       scanf("%d", &graus);
@@ -25,13 +21,7 @@ int main(){
 
   printf("O resultado da sua conversao e de: %d", convert);
 
-    printf("\nDigite quantos graus deseja converter de Fah para C: ");
-      scanf("%d", &fah);
-
-  convert2 = convertTempCelsius(fah);
-
-  printf("O resultado da sua conversao e de: %d", convert2);
-
+  
   
 
     return 0;
