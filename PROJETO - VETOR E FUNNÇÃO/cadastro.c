772a147c69ecfd64fca9fff259e679fdb746e115
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-05-14 17:07:31 -0300

    utilizando switch case e funções de veririfcações

--- PROJETO - VETOR E FUNNÇÃO/cadastro.c
+++ PROJETO - VETOR E FUNNÇÃO/cadastro.c
@@ -1,47 +1,94 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 char verificaEmail(char email[50]){
 if(strstr(email, "@")){
     printf("");
 }else{
-    printf("caracteres Invalidos");
+    printf("\t\t\t\tEmail Invalido\n");
+}
+
+}
+
+char verificaSexo(char sexo[20]){
+    
+    if(strcmp(sexo, "Masculino")){
+    printf("\t\t\t\tSexo Invalido\n");
+}else{
+    printf("");
+}
+
+}
+
+double verificaAltura( double altura){
+    if(altura >= 1 && altura <=2 ){
+  printf("");
+}else{
+  printf("\t\t\t\tAltura Invalida");
 }
 
 }
 
 
-char verificaSexo(char sexo[12]){
-if(strcmp(sexo, "Feminino")){
-  printf("Sexo Valido");
-} else{
-  printf("Sexo Invalido");
-}
-}
+
 
 int main(){
 
-char nome[200], email[50], sexo[12];
 
+
+char nome[100], email[50], sexo[20];
+char retorno;
+char endereco[100];
+double altura;
+int vacina;
+int opcao;
+
+
+printf("\t\t\t\t----------------------\n");
+printf("\t\t\t\t--- SEJA BEM VINDO ---\n");
+printf("\t\t\t\t----------------------\n");
+
+printf("\t\t\t\t1 - Iniciar\n");
+printf("\t\t\t\t0 - Sair\n");
+  scanf("%d", &opcao);
+
+  switch(opcao){
+    case 1:  
+printf("\t\t\t\t----------------\n");
+printf("\t\t\t\t--- CADASTRO ---\n");
+printf("\t\t\t\t----------------\n");
 
  
-printf("----------------\n");
-printf("--- CADASTRO ---\n");
-printf("----------------\n");
-
- 
-printf("Nome Completo: \n");
+printf("\t\t\t\tNome Completo: \n");
   gets(nome);
-printf("E-mail: \n");
+printf("\t\t\t\tE-mail: \n");
   gets(email);
-printf("Sexo: \n");
+printf("\t\t\t\tSexo: \n");
   gets(sexo);
+printf("\t\t\t\tEndereco: \n");
+  gets(endereco);
+printf("\t\t\t\tAltura: ");
+  scanf("%.2f", &altura);
+printf("\t\t\t\tVacina: \n");
+  printf("\t\t\t\t1 - Sim; 2 - Nao: \n");
+    scanf("%d", &vacina);
     
 
+
+retorno = verificaSexo(sexo);
+    printf("%c", retorno);
+
 printf(verificaEmail(email));
-printf(verificaSexo(sexo));
+
+printf("%.2f", verificaAltura(altura));;
+
+    break;
+case 0: ;
+break;
+  } 
+
+
 
 
 
