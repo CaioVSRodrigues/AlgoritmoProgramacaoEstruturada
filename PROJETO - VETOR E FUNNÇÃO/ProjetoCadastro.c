bd46e6e8b3d4fe46cd657a4e30dfd480752cddf9
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-05-23 19:49:58 -0300

    Incluindo Bibliotecas e Definindo as variáveis

---
+++ PROJETO - VETOR E FUNNÇÃO/ProjetoCadastro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+
+int totalUsuarios = 0;
+int ids[1000];
+char nomes[1000][50];
+char emails[1000][50];
+char sexos[1000][50];
+char enderecos[1000][100];
+float alturas[1000];
+char vacinas[1000][50];
