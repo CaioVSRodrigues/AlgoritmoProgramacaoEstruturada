d65ca63cb7c05f19d625fd45250a0b44852e9040
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-05-23 20:06:34 -0300

    Criando função para Adicionar usuários e gerar id

--- PROJETO - VETOR E FUNNÇÃO/ProjetoCadastro.c
+++ PROJETO - VETOR E FUNNÇÃO/ProjetoCadastro.c
@@ -13,3 +13,24 @@ char sexos[1000][50];
 char enderecos[1000][100];
 float alturas[1000];
 char vacinas[1000][50];
+
+int gerarId() {
+    srand(time(NULL));
+    return rand() % 1000 + 7;
+}
+
+void adicionarUsuario() {
+    if (totalUsuarios == 1000) {
+        printf("Limite maximo de usuarios alcancado.\n");
+        return;
+    }
+
+    int id = gerarId();
+    ids[totalUsuarios] = id;
+
+    printf("\n---- Adicionar Usuario ----\n");
+
+    printf("Nome completo: ");
+    fgets(nomes[totalUsuarios], 50, stdin);
+    nomes[totalUsuarios][strcspn(nomes[totalUsuarios], "\n")] = '\0';
+}
