92cd28c14f0350b4813487f3b68ad001f9bc31f7
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-05-23 20:09:38 -0300

    Criando funções de validações

--- PROJETO - VETOR E FUNNÇÃO/ProjetoCadastro.c
+++ PROJETO - VETOR E FUNNÇÃO/ProjetoCadastro.c
@@ -19,6 +19,46 @@ int gerarId() {
     return rand() % 1000 + 7;
 }
 
+
+int validarEmail(const char* email) {
+    int i;
+    int atCount = 0;
+    int dotCount = 0;
+
+    for (i = 0; email[i] != '\0'; i++) {
+        if (email[i] == '@') {
+            atCount++;
+        } else if (email[i] == '.') {
+            dotCount++;
+        }
+    }
+
+    if (atCount == 1 && dotCount >= 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+int validarSexo(const char* sexo) {
+    if (strcmp(sexo, "Feminino") == 0 || strcmp(sexo, "Masculino") == 0 || strcmp(sexo, "Indiferente") == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+int validarAltura(float altura) {
+    if (altura >= 1.0 && altura <= 2.0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 void adicionarUsuario() {
     if (totalUsuarios == 1000) {
         printf("Limite maximo de usuarios alcancado.\n");
@@ -33,4 +73,3 @@ void adicionarUsuario() {
     printf("Nome completo: ");
     fgets(nomes[totalUsuarios], 50, stdin);
     nomes[totalUsuarios][strcspn(nomes[totalUsuarios], "\n")] = '\0';
-}
